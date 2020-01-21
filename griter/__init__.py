c4b095336108d7da38fdcf39e9fd45b309b0137a
Jeff Anderson <peawormsworth@gmail.com>
2020-01-20 22:16:18 -0800

    step reversal made obvious

--- griter/__init__.py
+++ griter/__init__.py
@@ -22,7 +22,8 @@ for i in range(10):
 
 The primary purpose of these programs is to create counters with:
 - no binary shift manipulations
-- a regex style symbol manipulation counter using strings.
+- manipulate string symbols according to a grey code counter.
+- use regex for symbol manipulation
 - limit the steps on determining adjacent states
 
 There is a very simple alternate process that creates the grey code.
@@ -32,15 +33,24 @@ flip the bit to the left of the right most 1 (bit that is true).
 Following these two simple steps at each step will produce the
 grey code in the remaining bit locations of your string.
 
+It is not neccessary to convert greyscale to binary for incremental changes.
+
 The addition of a hidden bit on the right provides parity tracking so there is
 no reason to calculate it on each step. But also, the location of the bits on 
 the right side of the string tell you everything you need to know about how to 
 step forwards or backwards in the code without ever having to know that state of
 the left most bits.
 
+For that reason, I assumed there might be a single regex (regular expression) to
+describe grey code increment process. I didn't find it. But it might be possible.
+The process is just string manipulating based on its current state. A single
+regex should cover it. Let me know if you find it.
+
 These programs highlight these ideas.
 
-Output is in grey-code reflected binary.
+
+Input is not validated.
+Output are strings of grey-code reflected binary.
 
 author: Jeffrey B Anderson - truejeffanderson at gmail.com
 source: https://github.com/peawormsworth/PyGriter
@@ -48,13 +58,17 @@ source: https://github.com/peawormsworth/PyGriter
 """
 
 import re
-import unittest
 
 class Greystate ():
     """
 Greystate provides a symbol manipulation state machine.
-options: code=string, symbols=2 char string, direction=boolean
+options: code=string of symbols, symbols=2 char string, direction=boolean
 methods: inc(), dec()
+
+Warning: direction is a parity. If the parity is even, the count will
+proceed "forward", if it is odd it will count the other way on increments.
+However, forward and backwards are relative terms for a pattern that loops.
+ie: forward = backwards (at a different spot in the pattern)
     """
 
     flip = lambda m,x: m.symbols[x == m.symbols[0]]
@@ -64,8 +78,15 @@ methods: inc(), dec()
         self.code       = code + symbols[not direction]
         self.before_one = re.compile(r'^(.*)(.)(%s[^%s]*)$' % ((symbols[1],)*2))
 
-    def dec (self): self.go(False)
-    def inc (self): self.go(True )
+    def inc (self): self.go( 1)
+    def dec (self): self.go(-1)
+
+    def steps (self):
+        return [ self.flip_last, self.flip_before_last_one ]
+
+    # inc() and dec() are the same functions with steps processed in reverse...
+    def go (self,order=1):
+        for f in self.steps()[::order]: f()
 
     def flip_last (self):
         self.code = self.code[:-1] + self.flip(self.code[-1])
@@ -77,19 +98,12 @@ methods: inc(), dec()
         except IndexError:
             pass
 
-    def go (self, oriented=True):
-
-        if oriented: self.flip_last()
-        self.flip_before_last_one()
-        if not oriented: self.flip_last()
-
     def __str__ (self):
         return '%s' % self.code[:-1]
 
     def __repr__ (self):
         return "%r(code='%r',symbols='%r',direction='%r')" % (
-            type(self).__name__, self.code[:-1], self.symbols, not self.code[-1]
-        )
+            type(self).__name__, self.code[:-1], self.symbols, not self.code[-1])
 
     def __next__ (self):
         self.inc()
@@ -102,17 +116,30 @@ class Greystring ():
 Greystring provides a string symbol manipulation iterator.
 options: code=string, symbols=2 char string, direction=boolean
 usage: iterator
+
+During inc() the last character in the string is flipped first
+then the character before the last one set to symbol 2 is flipped
+if the whole string is set to symbol 1, this 2nd step is skipped.
+That is normal, because the parity is always flipped.
+So although the state does not change the parity does and the state is
+ready to continue the pattern on as a reflection of what it just did.
+the double beat in the pattern represents a location where the display
+has run out of characters to flip. The code would be changed at a bit
+that exceeds the display length. ie: this is normal for grey code.
     """
+
     flip = lambda m,x: m.symbols[x == m.symbols[0]]
 
-    def __init__ (self, code='000', symbols='01', direction=True):
+    def state  (self): return self.code[:-1]
+    def parity (self): return self.code[ -1]
+
+    def __init__ (self, code='000', symbols='01', parity=True):
         self.symbols    = symbols
-        self.code       = code + symbols[not direction]
+        self.code       = code + symbols[not parity]
         self.before_one = re.compile(r'^(.*)(.)(%s[^%s]*)$' % ((symbols[1],)*2))
 
     def __next__ (self):
-        state, parity = self.code[:-1], self.code[-1]
-        self.code = state + self.flip(parity)
+        self.code = self.state() + self.flip(self.parity())
         try:
             pre, char, post = self.before_one.findall(self.code)[0]
             self.code = pre + self.flip(char) + post
@@ -124,12 +151,11 @@ usage: iterator
         return self
 
     def __str__ (self):
-        return '%s' % self.code[:-1]
+        return '%s' % self.state()
 
     def __repr__ (self):
         return "%r(code='%r',symbols='%r',direction='%r')" % (
-            type(self).__name__, self.code[:-1], self.symbols, not self.code % 2
-        )
+            type(self).__name__, self.state(), self.symbols, self.parity())
 
 
 class Greylog ():
@@ -138,7 +164,12 @@ Greylog provides an integer manipulation iterator.
 options: code=int, parity=int
 note: code input is a raw integer value of the greycode state value.
 usage: iterator
+
+next() flips the last bit and then the bit just prior to the most right bit 
+set to 1. And n is largest power of 2 that divides the code evenly.
+aka: count of zeros on the right side of binary code
     """
+
     def __init__ (self, code=0, parity=0):
         self.code = code * 2 + parity
 
@@ -158,47 +189,6 @@ usage: iterator
 
     def __repr__ (self):
         return "%r(code='%r',parity='%r')" % (
-            type(self).__name__, self.code // 2, self.code % 2
-        )
+            type(self).__name__, self.code // 2, self.code % 2)
 
-
-
-class TestGreystring (unittest.TestCase):
-    verbose = 1
-    expect = """
-     000 001 011 010 110 111 101 100
-     100 101 111 110 010 011 001 000
-     000 001 011 010 110 111 101 100
-     100 101 111 110 010 011 001 000
-     000 001 011 010 110 111 101
-""".split()
-
-    def test_forward_reverse(m):
-        g = Greystring('000')
-        print('Check 35 strings starting from "%s"...' % (g))
-        for e in m.expect:
-            m.assertEqual(e,str(g))
-            if m.verbose: print('%8s' % (g))
-            g.inc()
-
-    def test_reverse(m):
-        #g = Greystring('101')
-        g = Greystring('101')
-        print('Check 35 strings back starting from "%s"...' % (g))
-        for e in m.expect[::-1]:
-            if m.verbose: print('%8s' % (g))
-            m.assertEqual(e,str(g))
-            g.dec()
-
-    def test_big_count(m):
-        g = Greystring('0'*20)
-        for i in range(78329):
-            print('g:',g)
-            g.inc()
-        print('final g:',g)
-
-
-if __name__ == '__main__':
-
-    unittest.main()
 
