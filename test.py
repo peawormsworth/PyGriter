7515280040463f35338bc770d86bc6307ea8826b
Jeff Anderson <peawormsworth@gmail.com>
2020-01-20 22:14:19 -0800

    minor testing

--- test.py
+++ test.py
@@ -1,15 +1,54 @@
 #!/usr/bin/python3
 from griter import *
+import unittest
 
-for object in (Greystring, Greylog, Greystate):
-#for object in (Greystring,):
-    x = object()
-    print(object.__name__)
-    for i in range(20):
-        print('%20s'%x)
-        next(x)
+verbose = 1
 
-exit;
+class TestComparison(unittest.TestCase):
+
+    bits = 18
+
+    def test_comparison(self):
+        """Compare output from classes run in parallel"""
+        a = Greystring('0'*self.bits)
+        b = Greystate ('0'*self.bits)
+        c = Greylog(0)
+
+        fmat = '%33s%33s%33s'
+        print(fmat % ('Greystate','Greystring','Greylog'))
+        if verbose: print(fmat % (a,b,c))
+        for i in range(2**self.bits-1):
+            next(a)
+            next(b)
+            next(c)
+            if verbose: print(fmat % (a,b,c))
+            lc = len(str(c))
+            self.assertEqual(str(a),str(b))
+            self.assertEqual(str(a)[-lc:],str(c))
+            self.assertEqual(str(b)[-lc:],str(c))
+
+
+# this isn't really a test at all
+# but it looks neat and it will freak out testers
+class TestSymbols(unittest.TestCase):
+
+    bits = 20 
+
+    def test_symbols(self):
+        """Alternate code symbols"""
+        a = Greylog()
+        b = Greystate('_'*self.bits,symbols='_8')
+        c = Greystring('|'*self.bits,symbols='|_')
+
+        fmat = '%33s%33s%33s'
+        for i in range(2**self.bits-1):
+            next(a)
+            next(b)
+            next(c)
+            if verbose: print(fmat % (a,b,c))
 
 
 
+if __name__ == '__main__':
+
+    unittest.main()
